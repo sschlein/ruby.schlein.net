c06916aebcc93e0245b8ecac8dbc5f234f1f2e2c
Sebastian Schlein <sebastian@schlein.net>
2021-01-03 14:50:55 +0100

    bla

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,28 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/RsXAdQAnKGg' frameborder='0' allowfullscreen></iframe></div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-8">
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/0v9ADTbNXhw' frameborder='0' allowfullscreen></iframe></div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-8">
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/678MCqnECl0' frameborder='0' allowfullscreen></iframe></div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-8">
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/aShS24L34sg' frameborder='0' allowfullscreen></iframe></div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-8">
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/bsryqFgAXPY' frameborder='0' allowfullscreen></iframe></div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-8">
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/mebqibHt170' frameborder='0' allowfullscreen></iframe></div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-8">
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/TFVTIRyOlaw' frameborder='0' allowfullscreen></iframe></div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-8">
+                <div class='embed-container'><iframe src='https://www.youtube.com/embed/qaj1-c9XNWg' frameborder='0' allowfullscreen></iframe></div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
 
         @livewireStyles
 
