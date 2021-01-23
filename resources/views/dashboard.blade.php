9efb97935434f4a1ccb603626a71e92fec0f19c3
Sebastian Schlein <sebastian@schlein.net>
2021-01-23 11:15:36 +0100

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <span>Noch keine Videos hochgeladen</span>
             @endif
             @foreach($videos as $video)
-                <div class="mb-2">Hochgeladen am {{ $video->created_at->format('m.d.y H:i') }} Uhr von {{ $video->user->name }}</div>
+                <div class="mb-2">Hochgeladen am {{ $video->created_at->format('d.m.y H:i') }} Uhr von {{ $video->user->name }}</div>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mb-8">
                 <div class='embed-container'><iframe src='{{ $video->youtube_url }}' frameborder='0' allowfullscreen></iframe></div>
             </div>
