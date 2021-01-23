e63f6dc8fd15c30e5e737ae1a5507b09ffe8e3ea
Sebastian Schlein <sebastian@schlein.net>
2021-01-23 14:34:24 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                     </div>
                 @endforeach
             </div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 {{ $videos }}
             </div>
         </div>
